f9215fc12630f2b84a9cf85ca1a0a53f804d5f1a
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2024-02-10 01:04:05 +0400

    fix: fix test issue

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,65 +1,9 @@
 import { simpleCalculator } from './index';
-import {
-  testCasesAdd,
-  testCasesDivide,
-  testCasesExponentiate,
-  testCasesInvalidAction,
-  testCasesInvalidArgs,
-  testCasesMultiply,
-  testCasesSubtract,
-} from './mocks';
+import { testCases } from './mocks';
 
 describe('simpleCalculator', () => {
-  it.each(testCasesAdd)(
-    'should add two numbers ($a, $b) and return $expected',
-    ({ a, b, action, expected }) => {
-      const mockInput = { a, b, action };
-      expect(simpleCalculator(mockInput)).toBe(expected);
-    },
-  );
-
-  it.each(testCasesSubtract)(
-    'should subtract two numbers ($a, $b) and return $expected',
-    ({ a, b, action, expected }) => {
-      const mockInput = { a, b, action };
-      expect(simpleCalculator(mockInput)).toBe(expected);
-    },
-  );
-
-  it.each(testCasesMultiply)(
-    'should multiply two numbers ($a, $b) and return $expected',
-    ({ a, b, action, expected }) => {
-      const mockInput = { a, b, action };
-      expect(simpleCalculator(mockInput)).toBe(expected);
-    },
-  );
-
-  it.each(testCasesDivide)(
-    'should divide two numbers ($a, $b) and return $expected',
-    ({ a, b, action, expected }) => {
-      const mockInput = { a, b, action };
-      expect(simpleCalculator(mockInput)).toBe(expected);
-    },
-  );
-
-  it.each(testCasesExponentiate)(
-    'should exponentiate two numbers ($a, $b) and return $expected',
-    ({ a, b, action, expected }) => {
-      const mockInput = { a, b, action };
-      expect(simpleCalculator(mockInput)).toBe(expected);
-    },
-  );
-
-  it.each(testCasesInvalidAction)(
-    'should return null for invalid action ($action)',
-    ({ a, b, action, expected }) => {
-      const mockInput = { a, b, action };
-      expect(simpleCalculator(mockInput)).toBe(expected);
-    },
-  );
-
-  it.each(testCasesInvalidArgs)(
-    'should return null for invalid arguments ($a, $b)',
+  test.each(testCases)(
+    'should return $expected when $a $action $b',
     ({ a, b, action, expected }) => {
       const mockInput = { a, b, action };
       expect(simpleCalculator(mockInput)).toBe(expected);
--- src/02-table-tests/mocks.ts
+++ src/02-table-tests/mocks.ts
@@ -7,44 +7,26 @@ type testCasesType = {
   expected: number | null;
 };
 
-export const testCasesAdd: testCasesType[] = [
+export const testCases: testCasesType[] = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
   { a: 2, b: 2, action: Action.Add, expected: 4 },
   { a: 3, b: 2, action: Action.Add, expected: 5 },
-];
-
-export const testCasesSubtract: testCasesType[] = [
   { a: 3, b: 2, action: Action.Subtract, expected: 1 },
   { a: 4, b: 1, action: Action.Subtract, expected: 3 },
   { a: 5, b: 5, action: Action.Subtract, expected: 0 },
-];
-
-export const testCasesMultiply: testCasesType[] = [
   { a: 3, b: 2, action: Action.Multiply, expected: 6 },
   { a: 4, b: 1, action: Action.Multiply, expected: 4 },
   { a: 5, b: 5, action: Action.Multiply, expected: 25 },
-];
-
-export const testCasesDivide: testCasesType[] = [
   { a: 6, b: 2, action: Action.Divide, expected: 3 },
   { a: 4, b: 1, action: Action.Divide, expected: 4 },
   { a: 10, b: 5, action: Action.Divide, expected: 2 },
-];
-
-export const testCasesExponentiate: testCasesType[] = [
   { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
   { a: 4, b: 1, action: Action.Exponentiate, expected: 4 },
   { a: 5, b: 3, action: Action.Exponentiate, expected: 125 },
-];
-
-export const testCasesInvalidAction: testCasesType[] = [
   { a: 3, b: 2, action: '%', expected: null },
   { a: 4, b: 1, action: '**', expected: null },
   { a: 5, b: 3, action: '=', expected: null },
-];
-
-export const testCasesInvalidArgs: testCasesType[] = [
-  { a: '3', b: '2', action: Action.Exponentiate, expected: null },
+  { a: '3', b: '2a', action: Action.Exponentiate, expected: null },
   { a: 'one', b: 'two', action: Action.Add, expected: null },
   { a: [1], b: [2], action: Action.Exponentiate, expected: null },
 ];
